59f1b6c6bfdb92c2d27352cb874e0c7d43224e73
LyrOwO <remichatenet@laposte.net>
2024-10-01 15:52:47 +0200

    Before tuto qui casse tout

---
+++ src/Controller/Admin/AuthorCrudController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Author;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class AuthorCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Author::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setSearchFields(['name', 'nickname', 'birthday'])
+        ;
+    }
+
+    
+
+
+    public function configureFields(string $pageName): iterable
+    {
+            yield TextField::new('name');
+            yield TextField::new('nickname');
+            yield DateField::new('birthday');
+    
+    
+    }
+    
+}
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -3,12 +3,15 @@
 namespace App\Entity;
 
 use App\Repository\AuthorRepository;
+use ApiPlatform\Metadata\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AuthorRepository::class)]
+/**A Author. */
+#[ApiResource]
 class Author
 {
     #[ORM\Id]
