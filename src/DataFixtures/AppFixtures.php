c2176aca3fe812657d99f2c44a7d21217cc4e158
LyrOwO <remichatenet@laposte.net>
2024-11-12 17:14:16 +0100

    before fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,17 @@
 namespace App\DataFixtures;
 
 use App\Entity\Product;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Finder\Finder;
 
-class AppFixtures extends Fixture
+class LoadData implements FixtureInterface
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
+        $finder = new Finder():
         // $product = new Product();
         // $manager->persist($product);
 
