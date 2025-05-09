144686150dbfec7f79114aac629ce6922b05134a
LyrOwO <remichatenet@laposte.net>
2025-05-09 14:01:46 +0200

    mieux

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,7 +14,6 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $user = $this->getUser();
-        dd($user); // Debug the logged-in user
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
